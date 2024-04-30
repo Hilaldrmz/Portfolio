319cf80683d4c784072546c6252c1deced2bd77d
hilaldrmz <hilalldurmaz@gmail.com>
2024-05-01 01:18:17 +0300

    Refactor image paths and remove unused assets function

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -9,7 +9,7 @@
                 <button class="small fw-sb" @click="goToProject(project.link)">View Project</button>
             </div>
             <div class="project-image">
-                <img :src="assets(project.image)" alt="Project Image">
+                <img :src="project.image" alt="Project Image">
             </div>
         </div>
     </div>
--- src/js/functions.js
+++
@@ -1,3 +0,0 @@
-export function assets(path) {
-    return import.meta.env.VITE_APP_BASE_URL + path;
-}
